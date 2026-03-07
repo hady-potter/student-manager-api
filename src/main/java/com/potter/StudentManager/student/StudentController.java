48627f8baf37ea2372475a46e5bdfc9460a303ad
hady-potter <hadym137@gmail.com>
2026-03-07 07:37:31 +0200

    Adding the CRUD operations in the student controller class

--- src/main/java/com/potter/StudentManager/student/StudentController.java
+++ src/main/java/com/potter/StudentManager/student/StudentController.java
@@ -1,9 +1,10 @@
 package com.potter.studentmanager.student;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,5 +14,33 @@ import java.util.List;
 public class StudentController {
 
 	private final StudentService service;
+
+	@GetMapping
+	public List<StudentDto> getAll() {
+		return service.getAll();
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<StudentDto> findById(@PathVariable String id) {
+		StudentDto foundStudent = service.findById(id);
+		return new ResponseEntity<>(foundStudent, HttpStatus.OK);
+	}
+
+	@PostMapping
+	public void add(@RequestBody StudentDto request) {
+		service.save(request);
+	}
+
+	@PutMapping("/update/{id}")
+	public void update(
+		@PathVariable(value = "id") String studentId,
+		@RequestBody StudentDto request) {
+		service.update(studentId, request);
+	}
+
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable(value = "id") String studentId) {
+		service.delete(studentId);
+	}
 
 }
